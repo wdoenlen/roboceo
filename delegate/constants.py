e5d04e4aef5bc179832bf5a42245b30b2d9f3ff8
wrd <wdoenlen@gmail.com>
2016-04-30 16:41:30 -0700

    Add environment variables to store secrets

---
+++ delegate/constants.py
@@ -0,0 +1,7 @@
+CEO_ACCESS_TOKEN = os.environ("CEO_ASANA_ACCESS_TOKEN")
+WORKSPACE_ID = int(os.environ("EXECUTIVE_MACHINE_ASANA_WORKSPACE_ID"))
+CEO_USER_ID = int(os.environ("CEO_ASANA_USER_ID"))
+WILL_USER_ID = int(os.environ("WILL_ASANA_USER_ID"))
+MAX_USER_ID = int(os.environ("MAX_ASANA_USER_ID"))
+MAX_EMAIL = os.environ("MAX_EMAIL")
+WILL_EMAIL = os.environ("WILL_EMAIL")
