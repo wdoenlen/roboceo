580f0a204182d0ae0f248f5f1df165c0e87cd2b2
wrd <wdoenlen@gmail.com>
2016-05-01 20:27:14 -0700

    Move files around

--- common.py
+++
@@ -1,6 +0,0 @@
-import asana
-import constants
-
-def build_asana_client():
-    """Connects to Asana and returns an API client"""
-    return asana.Client.access_token(constants.CEO_ACCESS_TOKEN)
--- constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CEO_ACCESS_TOKEN = os.environ.get("CEO_ASANA_ACCESS_TOKEN")
-WORKSPACE_ID = int(os.environ.get("EXECUTIVE_MACHINE_ASANA_WORKSPACE_ID"))
-CEO_USER_ID = int(os.environ.get("CEO_ASANA_USER_ID"))
-WILL_USER_ID = int(os.environ.get("WILL_ASANA_USER_ID"))
-MAX_USER_ID = int(os.environ.get("MAX_ASANA_USER_ID"))
-MAX_EMAIL = os.environ.get("MAX_EMAIL")
-WILL_EMAIL = os.environ.get("WILL_EMAIL")
