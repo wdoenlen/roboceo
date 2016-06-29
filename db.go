1d2077c02327a9ea0e5b474806a8a78e698e8ec2
Max Hawkins <maxhawkins@gmail.com>
2016-06-29 17:44:35 +0800

    use ql so it compile without cgo

--- db.go
+++ db.go
@@ -3,13 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"log"
 	"time"
 
 	"github.com/Masterminds/squirrel"
+	"github.com/cznic/ql"
 	"github.com/maxhawkins/eventscrape/graphapi"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type DB struct {
@@ -42,11 +40,22 @@ func (d *DB) SaveEvent(event graphapi.Event) error {
 		return err
 	}
 
-	_, err := d.db.Exec(`
-			INSERT OR REPLACE INTO events
+	tx, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec(`DELETE FROM events WHERE id == $1`, parsedEvent.ID)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(`
+			INSERT INTO events
 			(id, data, start_time, end_time, latitude, longitude)
 			VALUES
-			(?, ?, ?, ?, ?, ?)`,
+			($1, $2, $3, $4, $5, $6)`,
 		parsedEvent.ID,
 		string(*event),
 		time.Time(parsedEvent.StartTime).UTC(),
@@ -58,11 +67,18 @@ func (d *DB) SaveEvent(event graphapi.Event) error {
 		return err
 	}
 
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (d *DB) GetEvents(req GetEventsRequest) ([][]byte, error) {
-	query := squirrel.Select("data").From("events")
+	query := squirrel.
+		Select("data").
+		From("events").
+		PlaceholderFormat(squirrel.Dollar)
 
 	if len(req.Bounds) == 4 {
 		query = query.Where(squirrel.GtOrEq{"latitude": req.Bounds[0]})
@@ -78,7 +94,12 @@ func (d *DB) GetEvents(req GetEventsRequest) ([][]byte, error) {
 		query = query.Where(squirrel.Lt{"start_time": req.End.UTC()})
 	}
 
-	rows, err := query.RunWith(d.db).Query()
+	queryStr, queryArgs, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := d.db.Query(queryStr, queryArgs...)
 	if err != nil {
 		return nil, err
 	}
@@ -101,20 +122,35 @@ func (d *DB) GetEvents(req GetEventsRequest) ([][]byte, error) {
 }
 
 func NewDB(path string) (*DB, error) {
-	db, err := sql.Open("sqlite3", path)
+	ql.RegisterDriver()
+
+	db, err := sql.Open("ql", path)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS events (
-		id TEXT PRIMARY KEY,
-		data TEXT NOT NULL,
-		start_time TIMESTAMP,
-		end_time TIMESTAMP,
-		latitude DOUBLE,
-		longitude DOUBLE)`)
+	tx, err := db.Begin()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS events (
+		id string NOT NULL,
+		data string NOT NULL,
+		start_time time,
+		end_time time,
+		latitude float64,
+		longitude float64)`)
+	if err != nil {
+		return nil, err
+	}
+
+	tx.Exec(`CREATE UNIQUE INDEX IF NOT EXISTS ON events (id)`)
+	tx.Exec(`CREATE INDEX IF NOT EXISTS ON events (start_time, end_time, latitude, longitude)`)
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
 	}
 
 	return &DB{db}, nil
