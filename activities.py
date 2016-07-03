256a87c93258225ee800baddf811992721e30fb0
wrd <wdoenlen@gmail.com>
2016-07-03 14:33:14 +0900

    Add list of business activities

---
+++ activities.py
@@ -0,0 +1,51 @@
+engineering_activities = [
+    "Synergizing",
+    "Innovating",
+    "1:1-ing",
+    "Meeting",
+    "Planning",
+    "Analyzing",
+    "Circling back",
+    "Closing the loop",
+    "Dreaming",
+    "Optimizing",
+    "Evaluating",
+    "Deciding",
+    "Boot strapping",
+    "Iterating",
+    "Launching",
+    "Thinking",
+    "Brainstorming",
+]
+
+
+business_activities = [
+    "Communicating",
+    "Synergizing",
+    "Innovating",
+    "1:1-ing",
+    "Meeting",
+    "Greeting",
+    "Planning",
+    "Analyzing",
+    "Circling back",
+    "Closing the loop",
+    "Dreaming",
+    "Optimizing",
+    "Process building",
+    "Evaluating",
+    "Deciding",
+    "Investing",
+    "Researching",
+    "Flowing",
+    "Thinking",
+    "Brainstorming",
+]
+
+networking_activities = [
+    "Communicating",
+    "Meeting",
+    "Greeting",
+    "Recruiting",
+    "Prospecting",
+]
